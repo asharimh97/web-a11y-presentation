6c58caa4a5eb8a6d87a7f1865ddc179b3ef67c2e
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-03-26 21:37:22 +0700

    Add dummy slides

---
+++ src/slides/basic/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**
+ * Kalo di slide ini poinnya cuma 2:
+ * - Implementasi semantic tags
+ * - Implementasi WAI-ARIA tags untuk component yang enggak punya semantic tags
+ */
+
+function BasicConcept() {
+  return <div />;
+}
+
+export default BasicConcept;
---
+++ src/slides/core-concept/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/**
+ * Di slide ini kita ngomongin tentang core concept dari web accessibility.
+ * yaitu POUR -> Perceivable, Operable, Understandable, Robust.
+ */
+
+function CoreConcept() {
+  return <div />;
+}
+
+export default CoreConcept;
