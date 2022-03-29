c8c92aa9800f0eaf1dbdd04b86e342d031d89044
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-03-29 21:43:09 +0700

    Add POUR

---
+++ src/slides/pour/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Slide, Text, FlexBox, Box } from "spectacle";
+
+function Pour() {
+  return (
+    <Slide>
+      <FlexBox height="100%" flexDirection="column" alignItems="center">
+        <FlexBox justifyContent="center" marginY="32px">
+          <Text fontSize="64px" fontWeight="bold" textAlign="center">
+            Perceivable
+          </Text>
+        </FlexBox>
+        <FlexBox justifyContent="space-between" margin="32px">
+          <Text
+            fontSize="64px"
+            fontWeight="bold"
+            textAlign="center"
+            style={{ marginRight: "72px" }}
+          >
+            Robust
+          </Text>
+          <FlexBox
+            width="175px"
+            height="175px"
+            style={{ transform: "rotate(45deg)" }}
+            backgroundColor="transparent"
+          />
+          <Text
+            fontSize="64px"
+            fontWeight="bold"
+            textAlign="center"
+            style={{ marginLeft: "72px" }}
+          >
+            Operable
+          </Text>
+        </FlexBox>
+        <FlexBox justifyContent="center" marginY="32px">
+          <Text fontSize="64px" fontWeight="bold" textAlign="center">
+            Undersandable
+          </Text>
+        </FlexBox>
+      </FlexBox>
+    </Slide>
+  );
+}
+
+export default Pour;
