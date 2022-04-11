0bedf275e812591ea3ae3dc3dec16204dc1542b7
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-04-11 22:29:34 +0700

    Add detail to about slide

--- src/slides/about/index.js
+++ src/slides/about/index.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { Slide, Heading, FlexBox } from "spectacle";
+import { Slide, Heading, FlexBox, Notes, Appear, Text } from "spectacle";
 
 function AboutAlly() {
   return (
     <Slide>
       <FlexBox height="100%" flexDirection="column" alignItems="flex-start">
         <Heading fontSize="100px">a11y?</Heading>
+        <Appear>
+          <Text fontSize="32px">
+            The content should be available to everyone and the functionality is
+            operated by anyone, literally everyone
+          </Text>
+        </Appear>
       </FlexBox>
+      <Notes>
+        <ul>
+          <li>
+            Transisi dari slide sebelumnya, di sini bahas gimana a11y ini belum
+            populer
+          </li>
+          <li>
+            Komunitas dan resource belajar dalam bahasa Indonesia masih sedikit
+          </li>
+          <li>
+            Terus juga ngecek di beberapa website dari startup kuda yang belum
+            serve accessibility dengan baik, sadly termasuk stockbit hehe
+          </li>
+        </ul>
+      </Notes>
     </Slide>
   );
 }
