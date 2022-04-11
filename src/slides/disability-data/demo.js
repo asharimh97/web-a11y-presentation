e4f819db6526e432b6b73e1df657407fb1f9c63a
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-04-11 22:29:25 +0700

    Add detail about disability data

---
+++ src/slides/disability-data/demo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Slide, Heading, FlexBox, Notes } from "spectacle";
+
+function Demo() {
+  return (
+    <Slide>
+      <FlexBox height="100%" flexDirection="column" alignItems="flex-start">
+        <Heading fontSize="75px">let's do a mini demo?</Heading>
+      </FlexBox>
+      <Notes>
+        <p>Demoin gimana web diakses oleh orang dengan keterbatasan fisik</p>
+      </Notes>
+    </Slide>
+  );
+}
+
+export default Demo;
--- src/slides/disability-data/index.js
+++ src/slides/disability-data/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { FaWheelchair, FaEyeSlash } from "react-icons/fa";
 import { MdHearingDisabled, MdElderly } from "react-icons/md";
-import { Slide, FlexBox, Text, Heading } from "spectacle";
+import { Slide, FlexBox, Text, Heading, Notes } from "spectacle";
+import Demo from "./demo";
 
 function PeopleWithDisability() {
   const datas = [
@@ -33,28 +34,69 @@ function PeopleWithDisability() {
     return <Icon size="100px" style={{ marginBottom: "18px" }} />;
   };
   return (
-    <Slide>
-      <Heading textAlign="left" fontFamily="text" fontSize="56px">
-        By implementing a11y we will provide equal access to 22.8+ mio people
-        with disability
-      </Heading>
-      <FlexBox margin="auto">
-        {datas.map((data, index) => (
-          <FlexBox key={index} flexDirection="column" alignItems="center">
-            {renderIcon(data.icon)}
-            <Text textAlign="center" fontSize="32px">
-              {data.value}
-              <br />
-              {data.text}
-            </Text>
-          </FlexBox>
-        ))}
-      </FlexBox>
-      <Text fontSize="20px" fontStyle="italic">
-        Source: *Indonesian difable according to BPS (12.5%), **PERTUNI:
-        Prevalensi kebutaan di Indonesia =3%, ***ILO, Kemenkes
-      </Text>
-    </Slide>
+    <>
+      <Slide>
+        <Heading textAlign="left" fontFamily="text" fontSize="56px">
+          By implementing a11y we will provide equal access to 22.8+ mio people
+          with disability
+        </Heading>
+        <FlexBox margin="auto">
+          {datas.map((data, index) => (
+            <FlexBox key={index} flexDirection="column" alignItems="center">
+              {renderIcon(data.icon)}
+              <Text textAlign="center" fontSize="32px">
+                {data.value}
+                <br />
+                {data.text}
+              </Text>
+            </FlexBox>
+          ))}
+        </FlexBox>
+        <Text fontSize="20px" fontStyle="italic">
+          Source: *Indonesian difable according to BPS (12.5%), **PERTUNI:
+          Prevalensi kebutaan di Indonesia =3%, ***ILO, Kemenkes
+        </Text>
+        <Notes>
+          <ul>
+            <li>
+              Jelasing tentang disability itu ada yang situasional, temporary,
+              permanent
+            </li>
+            <li>
+              When people talk about accessibility, we tend to think about blind
+              people, but there's more about impairment, such user maybe only
+              using keyboard, head trace, some people maybe have broken wrist,
+              broken trackpad, or just shaking in the train
+            </li>
+            <li>
+              Ada juga user dengan cognitive impairment kaya dislexia, autism,
+              ADD, user-user ini butuh a diverse accommodation such as zoom in
+              the screen, minimal design to minimize distraction and cognitive
+              load
+            </li>
+            <li>
+              Many website are visual in their nature and lack keyboard
+              navigation which is essential for blind people to be able to
+              navigate through the content
+            </li>
+            <li>
+              Orang-orang dengan disabilitas ini seharusnya enggak ada kesulitan
+              untuk menikmati experience web kita
+            </li>
+            <li>
+              In summary making a website accessible is really about making sure
+              that the content and the website we create is usable to people
+              with various impairments or abilities
+            </li>
+            <li>
+              No matter what your product is there's a whole range of user out
+              there
+            </li>
+          </ul>
+        </Notes>
+      </Slide>
+      <Demo />
+    </>
   );
 }
 
