b9d0d0e158e3f1aee3d6809967d7436a9729e942
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-04-11 22:29:03 +0700

    Add notes to tech stack

--- src/slides/tech-stacks/index.js
+++ src/slides/tech-stacks/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Slide, FlexBox, Heading } from "spectacle";
+import { Slide, FlexBox, Notes } from "spectacle";
 import {
   SiReact,
   SiVuedotjs,
@@ -22,6 +22,15 @@ function TechStack() {
       <FlexBox alignItems="center" margin="auto" paddingBottom="56px">
         {stacks.map((stack) => renderIcon(stack))}
       </FlexBox>
+      <Notes>
+        <ul>
+          <li>
+            Bahas gimana di tengah-tengah topik "hot" tentang web yang isinya
+            terkait web framework atau blockchain dan web3, ada part dari web
+            development yang penting tapi enggak populer
+          </li>
+        </ul>
+      </Notes>
     </Slide>
   );
 }
