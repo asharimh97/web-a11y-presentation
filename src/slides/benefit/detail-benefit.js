de9020780fcaf0fabf1a9056feabe3a3792a384c
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-04-11 22:29:13 +0700

    Add detail benefit slide

---
+++ src/slides/benefit/detail-benefit.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Slide, Heading, FlexBox } from "spectacle";
+
+function DetailBenefit() {
+  return (
+    <Slide>
+      <FlexBox height="100%" flexDirection="column" alignItems="flex-start">
+        <Heading fontSize="64px">Boost SEO Performance</Heading>
+        <Heading fontSize="64px">Good a11y = good UX</Heading>
+        <Heading fontSize="64px">Potential users</Heading>
+      </FlexBox>
+    </Slide>
+  );
+}
+
+export default DetailBenefit;
--- src/slides/benefit/index.js
+++ src/slides/benefit/index.js
@@ -1,30 +1,35 @@
 import React from "react";
 import { Slide, Heading, FlexBox, Notes } from "spectacle";
+import DetailBenefit from "./detail-benefit";
 
 function Benefit() {
   return (
-    <Slide>
-      <FlexBox height="100%" flexDirection="column" alignItems="center">
-        <Heading margin="0px" fontSize="125px">
-          Benefit???
-        </Heading>
-      </FlexBox>
-      <Notes>
-        <ul>
-          <li>
-            Implementasi a11y akan ngeboost SEO, karena rulenya overlap sama SEO
-          </li>
-          <li>
-            Implementasi a11y yang baik secara enggak langsung juga ngasih user
-            experience yang baik
-          </li>
-          <li>
-            Potensi retention, dengan user merasa nyaman untuk pake appnya user
-            bisa jadi krasan dan kembali lagi
-          </li>
-        </ul>
-      </Notes>
-    </Slide>
+    <>
+      <Slide>
+        <FlexBox height="100%" flexDirection="column" alignItems="center">
+          <Heading margin="0px" fontSize="125px">
+            Benefit???
+          </Heading>
+        </FlexBox>
+        <Notes>
+          <ul>
+            <li>
+              Implementasi a11y akan ngeboost SEO, karena rulenya overlap sama
+              SEO
+            </li>
+            <li>
+              Implementasi a11y yang baik secara enggak langsung juga ngasih
+              user experience yang baik
+            </li>
+            <li>
+              Potensi retention, dengan user merasa nyaman untuk pake appnya
+              user bisa jadi krasan dan kembali lagi
+            </li>
+          </ul>
+        </Notes>
+      </Slide>
+      <DetailBenefit />
+    </>
   );
 }
 
