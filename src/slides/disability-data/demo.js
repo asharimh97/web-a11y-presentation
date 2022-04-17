150cc954a7b69b6e3568d0132fdcf31646188da6
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-04-17 22:36:35 +0700

    Change wording

--- src/slides/disability-data/demo.js
+++ src/slides/disability-data/demo.js
@@ -5,7 +5,7 @@ function Demo() {
   return (
     <Slide>
       <FlexBox height="100%" flexDirection="column" alignItems="flex-start">
-        <Heading fontSize="75px">let's do a mini demo?</Heading>
+        <Heading fontSize="75px">let's take a look?</Heading>
       </FlexBox>
       <Notes>
         <p>Demoin gimana web diakses oleh orang dengan keterbatasan fisik</p>
